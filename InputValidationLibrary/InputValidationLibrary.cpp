794eb5a815d9ac7dd5020908f660defa31948462
i3bdolah <xbdullahx@gmail.com>
2024-04-08 06:54:01 +0300

    Fixing main fun print

--- InputValidationLibrary/InputValidationLibrary.cpp
+++ InputValidationLibrary/InputValidationLibrary.cpp
@@ -10,6 +10,7 @@ int main()
 
 	cout << "Is 11/1/2024 Within 10/1/2024 and 10/5/2024 Period ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 1, 2024), Date(10, 5, 2024)) << endl;
 	cout << "Is 11/1/2024 Within 10/5/2024 and 10/1/2024 Period (Reversed Period) ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 5, 2024), Date(10, 1, 2024)) << endl;
+	cout << "Is 35/12/2022 a Valid Date ? = " << InputValidation::IsDateValid(Date(35, 12, 2022)) << endl;
 
 	printf("\nPlease Enter a Number : ");
 	int x = InputValidation::ReadInt();
@@ -17,8 +18,7 @@ int main()
 
 	printf("\nPlease Enter a Float Number between 1 and 5 : ");
 	float y = InputValidation::ReadFloatBetween(1, 5, "Number is not within range, Enter again : ");
-	cout << "y = " << y;
+	cout << "y = " << y << endl;
 
-	cout << endl << InputValidation::IsDateValid(Date(35, 12, 2022)) << endl;
 
 }
