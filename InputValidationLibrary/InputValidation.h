ac5fd411dd5858c7466d5e542234a7fd2c261abf
i3bdolah <xbdullahx@gmail.com>
2024-05-16 22:53:42 +0300

    Using Template Class

--- InputValidationLibrary/InputValidation.h
+++ InputValidationLibrary/InputValidation.h
@@ -3,19 +3,23 @@
 #include "Period.h";
 using namespace std;
 
+template <class T>
 class InputValidation
 {
 public:
-	static bool IsNumberBetween(int num, int from, int to) {
-		return ((num >= from) && (num <= to));
-	}
-	static bool IsNumberBetween(short num, short from, short to) {
-		return ((num >= from) && (num <= to));
-	}
-	static bool IsNumberBetween(float num, float from, float to) {
-		return ((num >= from) && (num <= to));
-	}
-	static bool IsNumberBetween(double num, double from, double to) {
+	//static bool IsNumberBetween(int num, int from, int to) {
+	//	return ((num >= from) && (num <= to));
+	//}
+	//static bool IsNumberBetween(short num, short from, short to) {
+	//	return ((num >= from) && (num <= to));
+	//}
+	//static bool IsNumberBetween(float num, float from, float to) {
+	//	return ((num >= from) && (num <= to));
+	//}
+	//static bool IsNumberBetween(double num, double from, double to) {
+	//	return ((num >= from) && (num <= to));
+	//}
+	static bool IsNumberBetween(T num, T from, T to) {
 		return ((num >= from) && (num <= to));
 	}
 	static bool IsDateBetween(Date date, Date fromDate, Date toDate) {
@@ -25,107 +29,107 @@ public:
 
 
 
-	static int ReadInt(string errorMsg = "Invalid Input, Re-Enter your number : ") {
-		int num;
+	//static int ReadInt(string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//	int num;
 
-		while (!(cin >> num))
-		{
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << errorMsg;
-		}
-		return num;
-	}
+	//	while (!(cin >> num))
+	//	{
+	//		cin.clear();
+	//		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	//		cout << errorMsg;
+	//	}
+	//	return num;
+	//}
 
-	static int ReadIntBetween(int from, int to,
-		string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//static int ReadIntBetween(int from, int to,
+	//	string errorMsg = "Invalid Input, Re-Enter your number : ") {
 
-		int num = ReadInt(errorMsg);
-		while (!IsNumberBetween(num, from, to)) {
-			cout << errorMsg;
-			num = ReadInt(errorMsg);
-		};
+	//	int num = ReadInt(errorMsg);
+	//	while (!IsNumberBetween(num, from, to)) {
+	//		cout << errorMsg;
+	//		num = ReadInt(errorMsg);
+	//	};
 
-		return num;
-	}
-
-
-
-	static short ReadShort(string errorMsg = "Invalid Input, Re-Enter your number : ") {
-		short num;
-
-		while (!(cin >> num))
-		{
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << errorMsg;
-		}
-		return num;
-	}
-
-	static short ReadShortBetween(short from, short to,
-		string errorMsg = "Invalid Input, Re-Enter your number : ") {
-
-		short num = ReadShort(errorMsg);
-		while (!IsNumberBetween(num, from, to)) {
-			cout << errorMsg;
-			num = ReadShort(errorMsg);
-		};
-
-		return num;
-	}
+	//	return num;
+	//}
 
 
 
-	static float ReadFloat(string errorMsg = "Invalid Input, Re-Enter your number : ") {
-		float num;
+	//static short ReadShort(string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//	short num;
 
-		while (!(cin >> num))
-		{
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << errorMsg;
-		}
-		return num;
-	}
+	//	while (!(cin >> num))
+	//	{
+	//		cin.clear();
+	//		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	//		cout << errorMsg;
+	//	}
+	//	return num;
+	//}
 
-	static float ReadFloatBetween(float from, float to,
-		string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//static short ReadShortBetween(short from, short to,
+	//	string errorMsg = "Invalid Input, Re-Enter your number : ") {
 
-		float num = ReadFloat(errorMsg);
-		while (!IsNumberBetween(num, from, to)) {
-			cout << errorMsg;
-			num = ReadFloat(errorMsg);
-		};
+	//	short num = ReadShort(errorMsg);
+	//	while (!IsNumberBetween(num, from, to)) {
+	//		cout << errorMsg;
+	//		num = ReadShort(errorMsg);
+	//	};
 
-		return num;
-	}
+	//	return num;
+	//}
 
 
 
-	static double ReadDouble(string errorMsg = "Invalid Input, Re-Enter your number : ") {
-		double num;
+	//static float ReadFloat(string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//	float num;
 
-		while (!(cin >> num))
-		{
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << errorMsg;
-		}
-		return num;
-	}
+	//	while (!(cin >> num))
+	//	{
+	//		cin.clear();
+	//		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	//		cout << errorMsg;
+	//	}
+	//	return num;
+	//}
 
-	static double ReadDoubleBetween(double from, double to,
-		string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//static float ReadFloatBetween(float from, float to,
+	//	string errorMsg = "Invalid Input, Re-Enter your number : ") {
 
-		double num = ReadDouble(errorMsg);
-		while (!IsNumberBetween(num, from, to)) {
-			cout << errorMsg;
-			num = ReadDouble(errorMsg);
-		};
+	//	float num = ReadFloat(errorMsg);
+	//	while (!IsNumberBetween(num, from, to)) {
+	//		cout << errorMsg;
+	//		num = ReadFloat(errorMsg);
+	//	};
 
-		return num;
-	}
+	//	return num;
+	//}
+
+
+
+	//static double ReadDouble(string errorMsg = "Invalid Input, Re-Enter your number : ") {
+	//	double num;
+
+	//	while (!(cin >> num))
+	//	{
+	//		cin.clear();
+	//		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	//		cout << errorMsg;
+	//	}
+	//	return num;
+	//}
+
+	//static double ReadDoubleBetween(double from, double to,
+	//	string errorMsg = "Invalid Input, Re-Enter your number : ") {
+
+	//	double num = ReadDouble(errorMsg);
+	//	while (!IsNumberBetween(num, from, to)) {
+	//		cout << errorMsg;
+	//		num = ReadDouble(errorMsg);
+	//	};
+
+	//	return num;
+	//}
 
 	static string ReadString() {
 		string input;
@@ -135,5 +139,28 @@ public:
 
 	static bool IsDateValid(Date date) {
 		return Date::IsDateValid(date);
+	}
+
+	static T ReadNum(string errorMsg = "Invalid Input, Re-Enter your number : ") {
+		T num;
+
+		while (!(cin >> num))
+		{
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << errorMsg;
+		}
+		return num;
+	}
+
+	static T ReadNumBetween(T from, T to, string errorMsg = "Invalid Input, Re-Enter your number : ") {
+		T num = ReadNum(errorMsg);
+		while (!IsNumberBetween(num, from, to)) {
+			cout << errorMsg;
+			num = ReadNum(errorMsg);
+		};
+
+		return num;
+
 	}
 };
--- InputValidationLibrary/InputValidationLibrary.cpp
+++ InputValidationLibrary/InputValidationLibrary.cpp
@@ -5,20 +5,24 @@ using namespace std;
 int main()
 {
 
-	cout << "Is Number 5 Between 1 and 10 ? = " << InputValidation::IsNumberBetween(5, 1, 10) << endl;
-	cout << "Is Number 5.4 Between 1.2 and 10.1 ? = " << InputValidation::IsNumberBetween(5.4, 1.2, 10.1) << endl;
+	//cout << "Is Number 5 Between 1 and 10 ? = " << InputValidation::IsNumberBetween(5, 1, 10) << endl;
+	//cout << "Is Number 5.4 Between 1.2 and 10.1 ? = " << InputValidation::IsNumberBetween(5.4, 1.2, 10.1) << endl;
 
-	cout << "Is 11/1/2024 Within 10/1/2024 and 10/5/2024 Period ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 1, 2024), Date(10, 5, 2024)) << endl;
-	cout << "Is 11/1/2024 Within 10/5/2024 and 10/1/2024 Period (Reversed Period) ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 5, 2024), Date(10, 1, 2024)) << endl;
-	cout << "Is 35/12/2022 a Valid Date ? = " << InputValidation::IsDateValid(Date(35, 12, 2022)) << endl;
+	//cout << "Is 11/1/2024 Within 10/1/2024 and 10/5/2024 Period ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 1, 2024), Date(10, 5, 2024)) << endl;
+	//cout << "Is 11/1/2024 Within 10/5/2024 and 10/1/2024 Period (Reversed Period) ? = " << InputValidation::IsDateBetween(Date(11, 1, 2024), Date(10, 5, 2024), Date(10, 1, 2024)) << endl;
+	//cout << "Is 35/12/2022 a Valid Date ? = " << InputValidation::IsDateValid(Date(35, 12, 2022)) << endl;
 
-	printf("\nPlease Enter a Number : ");
-	int x = InputValidation::ReadInt();
-	cout << "x = " << x << endl;
+	//printf("\nPlease Enter a Number : ");
+	//int x = InputValidation::ReadInt();
+	//cout << "x = " << x << endl;
 
-	printf("\nPlease Enter a Float Number between 1 and 5 : ");
-	float y = InputValidation::ReadFloatBetween(1, 5, "Number is not within range, Enter again : ");
-	cout << "y = " << y << endl;
+	//printf("\nPlease Enter a Float Number between 1 and 5 : ");
+	//float y = InputValidation::ReadFloatBetween(1, 5, "Number is not within range, Enter again : ");
+	//cout << "y = " << y << endl;
 
 
+	cout << "NUMBER : " << endl;
+	int num = InputValidation <int>::ReadNum();
+
+	cout << num;
 }
